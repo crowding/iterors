9002f523257c9ab92fb68cdba363fc23729cb4f4
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-13 13:45:21 +0000

    Testing izip against product.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/itertools/pkg@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- test/izipTest.R
+++ test/izipTest.R
@@ -9,3 +9,18 @@ test02 <- function() {
   expected <- list()
   checkEquals(actual, expected)
 }
+
+test03 <- function() {
+  x <- 1:3
+  y <- rnorm(5)
+  z <- letters[1:4]
+  nx <- length(x)
+  ny <- length(y)
+  nz <- length(z)
+  ix <- irep(x, each=ny*nz)
+  iy <- irep(y, times=nx, each=nz)
+  iz <- irep(z, times=nx*ny)
+  actual <- as.list(izip(a=ix, b=iy, c=iz))
+  expected <- as.list(product(a=x, b=y, c=z))
+  checkEquals(actual, expected)
+}
